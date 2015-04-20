73ef7e31d78cb71626947a0eb1229f1c888d0b8c
Nikhil Sahu <nikhilsahu.24@gmail.com>
2015-04-20 21:43:10 +0530

    about us

---
+++ aboutus.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('connectiondata.php');
+
+
+?>
+<html>
+<head>
+<title>iPortal - ABOUT</title>
+<link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+<div class="mainBody">
+<div class='header'>
+<img src="images/logo.png" height='100%'>
+<img src="images/trademark.gif" height='100%' style="margin-left:140px;margin-right:140px">
+<img src="images/iitd.png" height='100%'>
+</div>
+<?php include_once('footer.php'); ?>
+<div class="main" >
+<h4>Introduction</h4>This is an academic institution portal known by the name <b>zephyr</b>
+The product has the following functionalities from the end user point of view.
+<div class="info" >
+<dl>
+    <dt>1</dt>
+    <dd>The students has access to course resources.</dd>
+    <dt>2</dt>
+    <dd>The students are informed through announcements and notifications related to the courses and the institution.</dd>
+    <dt>3</dt>
+    <dd>The students can communicate with professors and fellow students.</dd>
+    <dt>4</dt>
+    <dd>They can participate and contribute to forum activities.</dd>
+    <dt>5</dt>
+    <dd>The faculty can upload resources related to the course.</dd>
+    <dt>6</dt>
+    <dd>Faculty can add announcements regarding the course.</dd>
+    <dt>7</dt>
+    <dd>Faculty can reply and help their students.</dd>
+</dl>
+</div>
+</div>
+</body>
+</html>
