4b6ed407c3e1a0186fa967ae55e05cbaef30d48c
himanshu godara <g.himanshu05@gmail.com>
2015-04-20 16:33:51 +0530

    othercourses update

--- menu.php
+++ menu.php
@@ -6,32 +6,32 @@
 			src="images/icons/profile1.png" class='topbaricon'
 			onmouseover="this.src='images/icons/profile3.png';"
 			onmouseout="this.src='images/icons/profile1.png';"
-			title='Profile' style="width:85%;height:8%"></a> 
+			title='Profile' style="width:85%;height:8%">Profile</a> 
 		  </li> 
            
 		   <li>  <a href='home.php'><img
 			src="images/icons/home1.png" class='topbaricon'
 			onmouseover="this.src='images/icons/home3.png';"
 			onmouseout="this.src='images/icons/home1.png';"
-			title='Home' style="width:85%;height:8%"></a> </li> 
+			title='Home' style="width:85%;height:8%">Home</a> </li> 
  
 		<li>  <a href='inbox.php'><img
 			src="images/icons/messages1.png" class='topbaricon'
 			onmouseover="this.src='images/icons/messages3.png';"
 			onmouseout="this.src='images/icons/messages1.png';"
-			title='Messages' style="width:85%;height:8%"></a> </li>  
+			title='Messages' style="width:85%;height:8%">Messages</a> </li>  
 			
 			<li> <a href='changePassword.php'><img
 			src="images/icons/settings1.png" class='topbaricon'
 			onmouseover="this.src='images/icons/settings3.png';"
 			onmouseout="this.src='images/icons/settings1.png';"
-			title='Settings' style="width:85%;height:8%"></a></li>
+			title='Settings' style="width:85%;height:8%">Settings</a></li>
 			
                  <li>   <a href='logout.php'><img
 			src="images/icons/logout1.png" class='topbaricon'
 			onmouseover="this.src='images/icons/logout3.png';"
 			onmouseout="this.src='images/icons/logout1.png';"
-			title='Logout' style="width:85%;height:8%"></a></li>  
+			title='Logout' style="width:85%;height:8%">Logout</a></li>  
 
 		 
                   </ul>
