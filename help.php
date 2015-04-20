525cbc50525d0afd9d259c9276b5fb5b3a4d5018
Nikhil Sahu <nikhilsahu.24@gmail.com>
2015-04-20 21:53:50 +0530

    help

---
+++ help.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>iPortal - help</title>
+<link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+<div class="mainBody">
+<div class='header'>
+<img src="images/logo.png" height='100%'>
+<img src="images/trademark.gif" height='100%' style="margin-left:140px;margin-right:140px">
+<img src="images/iitd.png" height='100%'>
+</div>
+<?php include_once('footer.php'); ?>
+<div class="help" >
+<h1 align="center">HELP</h1>
+<div class="units">
+<dl>
+    <dt><b>Messages</b></dt>
+    <dd>This is like a normal messenger where you can chat with the students of same course and faculty members.</dd>
+    <dt><b>Settings</b></dt>
+    <dd>The users can edit their password and add profile picture. </dd>
+    <dt><b>Profile</b></dt>
+    <dd>The user can view his entire profile in this. He can look at the courses he is enrolled in and can view his grades.</dd>
+    <dt><b>Notifications</b></dt>
+    <dd>The user is notified on this tab whenever there is any announcemnet regarding the courses he is enrolled in.</dd>
+    <dt><b>Add a course</b></dt>
+    <dd>There is a + tab on the home page, to add any course to the current enrolled courses.</dd>
+    <br>
+    <br>
+    Contact us at : <b>nikhilsahu.24@gmail.com</b>
+</dl>
+
+
+</div>
+
+</div>
+</body>
+</html>
